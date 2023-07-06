00400e69db4c3246170b3cd774ce6ac78aff99b7
yoonji811 <yoonji811@gmail.com>
2023-07-06 16:21:15 +0900

    remove train backbone shell script

--- shell/train_ood_backbone.sh
+++
@@ -1,7 +0,0 @@
-echo "Start OOD backbone (ResNet-50) train! \n"
-
-python ood/backbone.py \
---data_root ./data_example \
---backbone_weight ./ood/backbone/resnet_e100_data_example.pth
-
-echo "OOD backbone (ResNet-50) train Done! \n"
